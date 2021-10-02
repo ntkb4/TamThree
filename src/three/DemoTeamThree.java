b91442fc09c302af7acc636aff7a0095e76e6587
shahl <shahl@192.168.1.239>
2021-10-02 15:13:24 -0400

    check the changes

--- src/three/DemoTeamThree.java
+++ src/three/DemoTeamThree.java
@@ -1,14 +1,8 @@
 package three;
 
 public class DemoTeamThree {
-
-	
-	public static void teamMembers() {
-		
-		//Changes by A.F 
-		System.out.println(" Anwar, Idys, Ouail, Shala, Tatiana, Crystal");
-		System.out.println(" Welcome to the group");
-		
+	public static void addNum(int a, int b, int c) {
+		System.out.println(a + b + c);
 	}
 	public static void subNum(int e, int f, int g) {
 		 e=10;
@@ -16,6 +10,9 @@ public class DemoTeamThree {
 		 g=4;
 		
 		System.out.println(e-f-g);
+		
+		System.out.println("welcome to the team");
+		
 	}
 	
 
