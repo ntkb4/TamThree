4b8603fdd308feb039142198a43b2ad14e588eaa
new2 <new2@wael>
2021-10-02 13:52:37 -0400

    subNum

--- src/three/DemoTeamThree.java
+++ src/three/DemoTeamThree.java
@@ -4,4 +4,11 @@ public class DemoTeamThree {
 	public static void addNum(int a, int b, int c) {
 		System.out.println(a + b + c);
 	}
+	public static void subNum(int e, int f, int g) {
+		 e=10;
+		 f= 3;
+		 g=4;
+		
+		System.out.println(e-f-g);
+	}
 }
