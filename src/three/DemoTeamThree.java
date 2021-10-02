c987726667601af8a64b035d8705a8b8dd44b292
Tatiana <Tatiana@10.90.7.81>
2021-10-02 15:03:03 -0400

    Merge branch 'master' of https://github.com/ntkb4/TamThree.git
    Conflicts:
    	src/three/DemoTeamThree.java

--- src/three/DemoTeamThree.java
+++ src/three/DemoTeamThree.java
@@ -6,14 +6,28 @@ public class DemoTeamThree {
 public static void main (String [] args) {
 		
 		System.out.println("Demo Team Three");
-		
-		
+			
 	}
 
 
 	public static void addNum(int a, int b, int c) {
 		System.out.println(a + b + c);
 
+	}
 	
+	public static void teamMembers() {
+		
+		//Changes by A.F 
+		System.out.println(" Anwar, Idys, Ouail, Shala, Tatiana, Crystal");
+		System.out.println(" Welcome to the group");
+		
+	}
+	public static void subNum(int e, int f, int g) {
+		 e=10;
+		 f= 3;
+		 g=4;
+		
+		System.out.println(e-f-g);
+
 	}
 }
