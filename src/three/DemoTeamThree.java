458b4a766c110e573f5fcad2b875b07f2b71ab4a
Administrator <Administrator@Shams_Khalid.mshome.net>
2021-10-02 10:57:10 -0700

    Anwar's changes

--- src/three/DemoTeamThree.java
+++ src/three/DemoTeamThree.java
@@ -3,6 +3,15 @@ package three;
 public class DemoTeamThree {
 	public static void addNum(int a, int b, int c) {
 		System.out.println(a + b + c);
+		
+	}
+	
+	public static void teamMembers() {
+		
+		//Changes by A.F 
+		System.out.println(" Anwar, Idys, Ouail, Shala, Tatiana, Crystal");
+		System.out.println(" Welcome to the group");
+		
 	}
 	public static void subNum(int e, int f, int g) {
 		 e=10;
