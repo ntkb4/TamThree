853b91312aa70aef6d46d96d188b59a44307e6de
nicolemorales <nicolemorales@nicoles-air>
2021-10-02 14:02:38 -0400

    Hiii

--- src/three/DemoTeamThree.java
+++ src/three/DemoTeamThree.java
@@ -1,23 +1,9 @@
 package three;
 
 public class DemoTeamThree {
-	public static void addNum(int a, int b, int c) {
-		System.out.println(a + b + c);
-		
-	}
 	
-	public static void teamMembers() {
-		
-		//Changes by A.F 
-		System.out.println(" Anwar, Idys, Ouail, Shala, Tatiana, Crystal");
-		System.out.println(" Welcome to the group");
-		
-	}
-	public static void subNum(int e, int f, int g) {
-		 e=10;
-		 f= 3;
-		 g=4;
-		
-		System.out.println(e-f-g);
-	}
+		public static void Dog(int one , int two, int three) {	
+			System.out.println( one + two + three);
+		}
+	
 }
